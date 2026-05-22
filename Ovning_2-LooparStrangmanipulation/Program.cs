ff6c5708403e41ef14b7f080ceb2e63e2acec535
Javier Diaz Urbano <me@xavidiaz.com>
2026-05-22 11:00:48 +0200

    wip: Övning 2 - huvudmeny och CheckAge påbörjad

--- Ovning_2-LooparStrangmanipulation/Program.cs
+++ Ovning_2-LooparStrangmanipulation/Program.cs
@@ -1,1 +1,55 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public static class Menu
+{
+    public static void Run()
+    {
+        Console.WriteLine("Välkommen till Huvudmeny!");
+        while (true)
+        {
+            Console.WriteLine("Välja mellan");
+            Console.Write("(0)Anvsluta");
+            string val = Console.ReadLine() ?? "";
+            switch (val)
+            {
+                case "1":
+                    Console.Clear();
+                    CheckAge();
+                    break;
+                case "0":
+                    Console.Clear();
+                    Console.WriteLine("Hej då!");
+                    return;
+                default:
+                    Console.WriteLine("Felaktig Input!");
+                    break;
+            }
+        }
+    }
+    private static void CheckAge()
+    {
+        Console.Write("(U)ngdom eller (P)ensionär?");
+        string val = Console.ReadLine() ?? "";
+        switch (val)
+        {
+            case "u":
+                Console.WriteLine("Ungdom vält");
+                return;
+            case "P":
+                Console.WriteLine("Pensionär vält");
+                return;
+            default:
+                Console.WriteLine("Try againg");
+                break;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello from Main");
+
+        Menu.Run();
+    }
+
+}
